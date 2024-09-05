78209d371c35d6ea571e9c70dd6127fc66cca2b3
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2024-09-05 10:58:41 +0700

    test: add tests for puller

--- puller/primary_test.go
+++ puller/primary_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/kanthorlabs/kanthorq/core"
 	"github.com/kanthorlabs/kanthorq/entities"
 	"github.com/kanthorlabs/kanthorq/pkg/pgcm"
@@ -74,7 +75,58 @@ func TestPrimary_Do(t *testing.T) {
 	for _, task := range second.Tasks {
 		require.Equal(t, entities.StateRunning, task.State)
 	}
+}
 
+func TestPrimary_Do_LockFailure(t *testing.T) {
+	ctx := context.Background()
+	conn, err := tester.SetupPostgres(ctx)
+	defer func() {
+		require.NoError(t, conn.Close(ctx))
+	}()
+	require.NoError(t, err)
+
+	cm, err := pgcm.New(os.Getenv("KANTHORQ_POSTGRES_URI"))
+	require.NoError(t, err)
+
+	res, err := core.DoWithCM(ctx, &core.ConsumerRegisterReq{
+		StreamName:                xfaker.StreamName(),
+		ConsumerName:              xfaker.ConsumerName(),
+		ConsumerSubjectIncludes:   []string{xfaker.Subject()},
+		ConsumerAttemptMax:        xfaker.F.Int16Between(2, 10),
+		ConsumerVisibilityTimeout: xfaker.F.Int64Between(15000, 300000),
+	}, cm)
+	require.NoError(t, err)
+
+	// use another connection to lock consumer
+	tx, err := conn.BeginTx(ctx, pgx.TxOptions{})
+	require.NoError(t, err)
+	_, err = (&core.ConsumerLockReq{Name: res.ConsumerRegistry.Name}).Do(ctx, tx)
+	require.NoError(t, err)
+
+	count := xfaker.F.IntBetween(101, 199)
+	events := tester.FakeEvents(xfaker.SubjectWihtPattern(res.ConsumerRegistry.SubjectIncludes[0]), count)
+
+	_, err = core.DoWithCM(ctx, &core.StreamPutEventsReq{
+		Stream: res.StreamRegistry,
+		Events: events,
+	}, cm)
+	require.NoError(t, err)
+
+	in := PullerIn{
+		Size:        100,
+		WaitingTime: 3000,
+	}
+	p := New(cm, res.StreamRegistry, res.ConsumerRegistry, in)
+
+	first, err := p.Do(ctx)
+	require.NoError(t, err)
+
+	// no data because of lock failure
+	require.Equal(t, 0, len(first.Tasks))
+	require.Equal(t, 0, len(first.Events))
+	require.Equal(t, 0, len(first.EventIds))
+
+	require.NoError(t, tx.Rollback(ctx))
 }
 
 func TestPrimary_Do_NoEvent(t *testing.T) {
